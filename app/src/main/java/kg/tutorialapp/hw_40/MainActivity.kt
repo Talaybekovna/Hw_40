f55352e98689177d48d4a5097fe42fed57b341e7
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-26 20:05:40 +0600

    add three fragments

--- app/src/main/java/kg/tutorialapp/hw_40/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/hw_40/MainActivity.kt
@@ -2,10 +2,16 @@ package kg.tutorialapp.hw_40
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
     }
 }
