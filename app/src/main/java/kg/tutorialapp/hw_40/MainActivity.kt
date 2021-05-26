8c9b945d649eaa87a52b24d5ae902c9ee7f1399f
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-26 20:29:33 +0600

    add Toolbar options menu

--- app/src/main/java/kg/tutorialapp/hw_40/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/hw_40/MainActivity.kt
@@ -2,16 +2,42 @@ package kg.tutorialapp.hw_40
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var tvText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvText = findViewById(R.id.tv_text)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate((R.menu.menu_item), menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val fragmentA = FragmentA()
+        val fragmentB = FragmentB()
+        val fragmentC = FragmentC()
+
+        when (item.itemId) {
+            R.id.item_a -> openFragment(fragmentA)
+            R.id.item_b -> openFragment(fragmentB)
+            R.id.item_c -> openFragment(fragmentC)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openFragment(fragment: Fragment) : Fragment {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+        return fragment
     }
 }
